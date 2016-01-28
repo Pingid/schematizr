79d1e70b29d77dca654fa27a633efe912fce5869
Daniel Beaven <dan@Daniels-MacBook-Air.local>
2016-01-28 13:24:40 +0000

    added ignore files, changed output folder. added es6 babel support. added some tests. commented code

--- src/schema.js
+++ src/schema.js
@@ -1,9 +1,12 @@
 import _ from 'ramda';
 
 // utils -----------------------------------------------------------------------
+// Blobal variable for the unique id's and function which returns in incremented
 let idCounter = 1;
 const uniqueId = () => { return idCounter++ }
 
+// Checks wether the item handed to it is an array plain object or other
+// returns the item wrapped in either an array callback, object callback or just the item
 const objectType = _.curry((objFunc, arrFunc, json) => {
   arrFunc = arrFunc || objFunc
   if(_.isArrayLike(json)) return arrFunc(json);
@@ -11,22 +14,28 @@ const objectType = _.curry((objFunc, arrFunc, json) => {
   return json;
 })
 
+// Recursive function which takes a function and nested json and iterates over
+// the json running every value through the function
 const iterator = _.curry((func, json) => {
   const recurse = _.compose(iterator(func), func);
   const iter = objectType(_.mapObjIndexed(recurse), _.map(recurse));
   return iter(json)
 })
 
+// Takes a filter function which must return a boolean and some nested json
+// it iterates over the json running each value through the filter
 const filterer = (filter, json) => {
   const filtered = iterator(x => objectType(filter, filter, x))
   return filtered(json)
 }
 
+// Remove any nulls undifines or false booleans
 const removeNull = (json) => {
   const filter = _.filter(x => x ? true : false);
   return filterer(filter, json)
 }
 
+// Remove any empty objects or arrays
 const removeEmpty = (json) => {
   const filter = _.filter(x => !_.isEmpty(x))
   return filterer(filter, json)
@@ -41,7 +50,7 @@ export const assemble = (json) => {
 // disassemble -----------------------------------------------------------------
 export const disassemble = (json) => {
   const removeId = objectType((obj) => _.dissoc('_id', obj), x => x)
-  return _.compose(removeEmpty, removeNull, iterator)(removeId, json)
+  return _.compose(removeEmpty, removeNull, iterator)(removeId, removeOuter)
 }
 
 // FindById --------------------------------------------------------------------
--- test/index.js
+++ test/index.js
@@ -1,12 +1,55 @@
 'use strict';
 
+var _ = require('ramda');
 var expect = require('chai').expect;
-var schematizr = require('../dest/schema'),
+var should = require('chai').should();
+
+var schematizr = require('../lib/schema'),
     assemble = schematizr.assemble,
     disassemble = schematizr.disassemble,
     findById = schematizr.findById,
     find = schematizr.find;
 
-describe('schematizr', function() {
+var testData = {
+  todoList: [
+    {  text: 'Exercise',
+      subList: [
+        { text: '5k run' },
+        { test: '30min stretch' }
+      ]
+    },
+    { text: 'Trim nose hairs' }
+  ]
+}
 
+describe('schematizr', function() {
+  var schema = assemble(testData)
+  var disassembled = disassemble(schema)
+  var found = findById((x) => 'FOUND', schema)
+  console.log(JSON.stringify(schema, null, 2));
+  console.log(JSON.stringify(disassembled, null, 2));
+  // console.log(JSON.stringify(found(2), null, 2));
+  describe('assemble', function() {
+    it('Outer object has _id key', function() {
+      expect(schema).to.include.keys('_id')
+    })
+    it('Nested objects have _id keys', function(){
+      expect(schema.todoList[0]).to.include.keys('_id')
+      expect(schema.todoList[1]).to.include.keys('_id')
+      expect(schema.todoList[0].subList[0]).to.include.keys('_id')
+      expect(schema.todoList[0].subList[1]).to.include.keys('_id')
+    })
+  })
+
+  describe('disassemble', function() {
+    it('Disassembled is equal to original', function(){
+      expect(disassembled).to.satisfy(_.equals(testData))
+    })
+  })
+
+  // describe('findById', function() {
+  //   it('Should return FOUND', function() {
+  //     expect(found(2)).to.satisfy(functio(x){ return x.todoList === 'FOUND'})
+  //   })
+  // })
 })
